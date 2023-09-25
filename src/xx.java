94f0ec01215416c4d132fae52a65789bae43c8d9
DESKTOP-JL8J59F\user1 <khbkhb85098509@gmail.com>
2023-09-25 19:12:01 +0900

    9월 25일 문제풀이 입니다.

--- src/xx.java
+++ src/xx.java
@@ -8,36 +8,17 @@ import java.util.Random;
 
 public class xx {
     public static void main(String[] args) {
-//        SwingUtilities.invokeLater(() -> {
-//            JFrame frame = new JFrame("Display Image Example");
-//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//            ImageIcon imageIcon = new ImageIcon("D:\\포카리스웨트.jpg"); // 이미지 파일의 경로를 지정해야 합니다.
-//            JLabel label = new JLabel(imageIcon);
-//
-//            frame.getContentPane().add(label);
-//            frame.pack();
-//            frame.setVisible(true);
-//        });
-//    }
-//
-        String[] youtubeLinks = {
-                "https://www.youtube.com/watch?v=RGLrL2M5MOs",
-                "https://www.youtube.com/watch?v=MHokxaunuJY",
-                "https://www.youtube.com/watch?v=9V2tVurYTxc",
-                "https://www.youtube.com/watch?v=vloMDezTHWc",
-                "https://www.youtube.com/watch?v=okVTSehE414"
-        };
+        Tv t1 = new Tv();
+        Tv t2 = new Tv();
+        System.out.println(t1.channel);
+        System.out.println(t2.channel);
 
-        Random random = new Random();
-        int randomIndex = random.nextInt(youtubeLinks.length);
-        System.out.println(randomIndex);
-        String selectedLink = youtubeLinks[randomIndex];
+        t1.channel = 7;
+        System.out.println("변경완료");
 
-        try {
-            Desktop.getDesktop().browse(new URI(selectedLink));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println(t1.channel);
+        System.out.println(t2.channel);
+
+
     }
 }
